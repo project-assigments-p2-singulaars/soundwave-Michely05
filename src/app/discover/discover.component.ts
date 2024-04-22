a80ee422e6945640be1ee01e1fa0a7c02b2365c2
[Michely Paredes Galeano] <[michelyparedes@gmail.com]>
2024-04-22 23:53:19 +0200

    positioned elements on discover page

--- src/app/discover/discover.component.ts
+++ src/app/discover/discover.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-discover',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './discover.component.html',
-  styleUrl: './discover.component.scss'
+  styleUrl: './discover.component.scss',
 })
-export class DiscoverComponent {
-
-}
+export class DiscoverComponent {}
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
