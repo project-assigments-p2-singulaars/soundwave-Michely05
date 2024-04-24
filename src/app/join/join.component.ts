0658de272970e8aafdcc72f03a109d35ffc08de6
[Michely Paredes Galeano] <[michelyparedes@gmail.com]>
2024-04-24 23:04:14 +0200

    finished join page, form missing

--- src/app/join/join.component.ts
+++ src/app/join/join.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-join',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './join.component.html',
-  styleUrl: './join.component.scss'
+  styleUrl: './join.component.scss',
 })
-export class JoinComponent {
-
-}
+export class JoinComponent {}
